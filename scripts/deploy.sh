f9ad9c6c3faa63fd4e5e80fca4524acad9905de5
poow810 <poow810@naver.com>
2023-10-11 14:33:05 +0900

    release demo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,19 @@
 REPOSITORY=/home/ubuntu/app
 cd $REPOSITORY
 
+echo "> Build 파일 복사"
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo ">현재 구동중인 애플리케이션 pid 확인"
+
 APP_NAME=demo
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+#JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID=$(pgrep -fl $APP_NAME | grep jar | awk '{print $1}')
 
-if [ -z $CURRENT_PID ] #2
-then
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $CURRENT_PID"
@@ -17,6 +22,11 @@ else
 fi
 
 echo "> $JAR_PATH 배포" #3
+# shellcheck disable=SC2012
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+echo "> JAR Name: $JAR_NAME"
+echo "> $JAR_NAME 에 실행 권한 추가"
+chmod +x "$JAR_NAME"
 nohup java -jar \
         -Dspring.profiles.active=dev \
-        build/libs/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+        build/libs/"$JAR_NAME" > $REPOSITORY/nohup.out 2>&1 &
