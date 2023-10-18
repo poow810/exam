89f55dee4c89cec38f705f7e89901747a169a555
poow810 <poow810@naver.com>
2023-10-18 19:26:51 +0900

    ec2 instance test

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class controller {
-
-    @Value("${app.version:1.0.0}")
-    String version;
-
     @GetMapping("/")
     public String home() {
-        return "Hello, CICD! version " + version;
+        return "Hello, CICD! ";
     }
 }
